a3bb317702ba86cf6af20ee6262fb1d965401552
Kwanpeemai <kwanpuengchai@gmail.com>
2025-08-09 19:31:38 +0700

    feat: Add authentication for admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :articles, only: [ :index, :show ]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
     resources :articles
     root "articles#index"
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  # root "posts#index"
   root "articles#index"
 end
